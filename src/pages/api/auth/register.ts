e37432a975599c46c69fa5859a0453821bb8a754
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-10-02 03:05:53 +0900

    작성자 표시

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -5,18 +5,31 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
-  
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+  const username = formData.get("username")?.toString();
+
+  if (!email || !password || !username) {
+    return new Response("Email, password, and username are required", { status: 400 });
   }
 
-  const { error } = await supabase.auth.signUp({
+  const { data, error: signUpError } = await supabase.auth.signUp({
     email,
     password,
   });
 
-  if (error) {
-    return new Response(error.message, { status: 500 });
+  if (signUpError) {
+    return new Response(signUpError.message, { status: 500 });
+  }
+
+  if (data.user) {
+    const { error: profileError } = await supabase
+        .from('profiles')
+        .insert([{ 'user_id': data.user.id, username }]);
+
+    if (profileError) {
+      return new Response(profileError.message, { status: 500 });
+    }
+  } else {
+    return new Response("User data is null", { status: 500 });
   }
 
   return redirect("/signin");
